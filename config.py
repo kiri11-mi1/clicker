3a93d384d00be1bec8ecd84a51936e66e974aad4
Kirill Mil <kirill.mil.0310@mail.ru>
2021-01-12 09:18:03 +0700

    initial

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    scrn_filename = 'screen.jpg'
+    button_image_path = os.path.join('img', 'button_2.png')
+    TOKEN = os.environ.get('TOKEN')
+    CHAT_ID = int(os.environ.get('CHAT_ID'))
